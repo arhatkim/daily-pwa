787b274297cd21b465ca0d977a2d320fa4362e38
arhatkim <163545334+arhatkim@users.noreply.github.com>
2025-11-08 08:21:21 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,20 @@
 const CACHE_NAME = 'daily-pwa-cache-v1';
 const urlsToCache = [
-  '/daily-pwa/',
-  '/daily-pwa/index.html',
-  '/daily-pwa/manifest.json',
-  '/daily-pwa/icon-192.png',
-  '/daily-pwa/icon-512.png'
+  './',
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
